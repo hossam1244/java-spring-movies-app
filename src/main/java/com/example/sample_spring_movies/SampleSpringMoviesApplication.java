3a9ee4b727f736d1a82649ec07cfd20299970ead
Hossam Rakha <hossamrakha0@gmail.com>
2024-05-12 16:19:12 +0300

    init project..

---
+++ src/main/java/com/example/sample_spring_movies/SampleSpringMoviesApplication.java
@@ -0,0 +1,14 @@
+package com.example.sample_spring_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringMoviesApplication.class, args);
+		System.out.println("spring boot application!!");
+	}
+
+}
